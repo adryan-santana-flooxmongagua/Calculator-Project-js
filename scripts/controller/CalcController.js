4d43debe3b80976a6440c37b3a911d0475f34f6a
Adryan Alex. <adryan.silva.flooxmongagua@gmail.com>
2025-01-23 13:44:22 -0300

    TimeDate

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,7 +2,10 @@
 class CalcController {
     constructor(){
 
-        this._displayCalc = "0";
+        this._locale = 'pt-BR';
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
 
@@ -10,37 +13,71 @@ class CalcController {
 
     initialize(){
 
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let timeEl = document.querySelector("#hora");
+        this.setDisplayDateTime();
 
-        displayCalcEl.innerHTML = "4566";
-        dateEl.innerHTML ="16/02/2007";
-        timeEl.innerHTML = "04:00";
+        let interval = setInterval(() => {
+
+             this.setDisplayDateTime();
+
+        }, 1000);
+
+      //  setTimeout(()=>{
+
+     //       clearInterval(interval);
+
+    //  },10000);  para definir quando ele deve parar de rodar o setTimeout
 
     }
+
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this.locale,{
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this.locale);
+
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    set displayTime(value){
+        return this._timeEl.innerHTML = value;
+    }
+    
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+    set displayDate(value){
+        return this._dateEl.innerHTML = value;
+    }
+    
 
     get displayCalc(){
 
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
 
     }
 
-    set displayCalc(valor){
+    set displayCalc(value){
 
-        this._displayCalc = valor;
+        this._displayCalcEl.innerHTML = value;
 
     }
 
     get currentDate(){
 
-        return this._currentDate;
+        return new Date();
 
     }
 
-    set currentDate(valor){
+    set currentDate(value){
 
-        this._currentDate = valor;
+        this._currentDate = value;
 
     }
 
